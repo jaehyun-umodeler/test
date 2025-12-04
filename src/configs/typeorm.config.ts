4e27b3e1135aee10c45c2f12379b5f61a67e124c
jaehyun-umodeler <jaehyun.p@umodeler.com>
2025-12-04 10:41:51 +0900

    test22

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -4,23 +4,36 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export default registerAs(
   'typeorm',
-  (): TypeOrmModuleOptions => ({
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: [join(__dirname, '/../**/*.entity*{.js,.ts}')],
-    migrations: [join(__dirname, '/../migrations/**/*{.js,.ts}')],
-    migrationsTableName: 'migrations',
-    logging: false,
-    synchronize: false,
-    migrationsRun: true,
-    extra: {
-      connectionLimit: 100,
-    },
-    autoLoadEntities: true,
-    timezone: 'Z',
-  }),
+  (): TypeOrmModuleOptions => {
+    const dbHost = process.env.DB_HOST;
+    const baseConfig: TypeOrmModuleOptions = {
+      type: 'mysql',
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [join(__dirname, '/../**/*.entity*{.js,.ts}')],
+      migrations: [join(__dirname, '/../migrations/**/*{.js,.ts}')],
+      migrationsTableName: 'migrations',
+      logging: false,
+      synchronize: false,
+      migrationsRun: true,
+      extra: {
+        connectionLimit: 100,
+      },
+      autoLoadEntities: true,
+      timezone: 'Z',
+    };
+
+    if (process.env.NODE_ENV === 'local') {
+      return {
+        ...baseConfig,
+        host: dbHost,
+        port: parseInt(process.env.DB_PORT || '3306', 10),
+      };
+    }
+    return {
+      ...baseConfig,
+      socketPath: dbHost,
+    };
+}
 );
